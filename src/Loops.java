c0711ce6881321b2e3d4d38eccb939d9f166cb5c
Will-R-UC <rahtzwd@mail.uc.edu>
2025-09-29 19:36:51 -0400

    Rename Main class to Loops

--- src/Loops.java
+++ src/Loops.java
@@ -1,10 +1,11 @@
-public class Main {
+public class Loops {
     public static void main(String[] args) {
         //Task 1
         System.out.println("# Task 1");
         for (int i = 0; i <= 30; i++) {
             System.out.print(i + " ");
         }
+
 
     }
 }
