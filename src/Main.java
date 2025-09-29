d9790ff948461851172bb84f274e18a985539c81
Will-R-UC <rahtzwd@mail.uc.edu>
2025-09-29 19:35:45 -0400

    Add task 1 loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        //Task 1
+        System.out.println("# Task 1");
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
 
     }
 }
