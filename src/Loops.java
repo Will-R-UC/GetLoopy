cb697a60c69cd95528db28f53f8be9cc34743e15
Will-R-UC <rahtzwd@mail.uc.edu>
2025-09-29 19:49:17 -0400

    Add task 4 code

--- src/Loops.java
+++ src/Loops.java
@@ -21,5 +21,13 @@ public class Loops {
         for (int i = 0; i <= 18; i += 3) {
             System.out.print(i + " ");
         }
+
+        System.out.print("\n\n");
+
+        //Task 4
+        System.out.println("# Task 4");
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
+        }
     }
 }
