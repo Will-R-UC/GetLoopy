0ec96735947cfcfea9efc73e7f9c98070e7cf6bc
Will-R-UC <rahtzwd@mail.uc.edu>
2025-09-29 21:02:23 -0400

    Add task 7 code

--- src/Loops.java
+++ src/Loops.java
@@ -51,5 +51,16 @@ public class Loops {
             }
             System.out.print("\n");
         }
+
+        System.out.print("\n\n");
+
+        //Task 7
+        System.out.println("# Task 7");
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
     }
 }
